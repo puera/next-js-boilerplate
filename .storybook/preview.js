77cf65a0a03f412369a93a28879cb676c6562d36
Renann Souza <renann_puera@hotmail.com>
2021-01-11 16:43:16 -0300

    Add decorator storybook-addon-next-router

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,8 @@
+import { addDecorator } from '@storybook/react'
+import { withNextRouter } from 'storybook-addon-next-router'
 import GlobalStyles from '../src/styles/global'
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+addDecorator(withNextRouter())
 
 export const decorators = [
   (Story) => (
